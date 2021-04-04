5782f3bb6e19a13200d8cff22e619f3ae4e6ffd9
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-04-04 20:13:10 +0700

    first commit

---
+++ QLSV_3Layer/CSDL_OOP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSV_3Layer
+{
+    class CSDL_OOP
+    {
+        public static CSDL_OOP Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new CSDL_OOP();
+                }
+                return _Instance;
+            }
+            private set
+            {
+
+            }
+        }
+        private static CSDL_OOP _Instance;
+    }
+}
